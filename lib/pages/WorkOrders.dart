d4a9592bbb22c5bec9ee6c7a3b4de10f1f454b78
Julio Cesar <jassito33c@gmail.com>
2023-11-27 02:05:08 -0600

    print eliminado

--- lib/pages/WorkOrders.dart
+++ lib/pages/WorkOrders.dart
@@ -300,7 +300,7 @@ class WorkOrderCard extends StatelessWidget {
                   ),
                 ],
               ),
-              subtitle: Text(longitude + ',' + latitude + woDescription),
+              subtitle: Text(woDescription),
             ),
           ],
         ),
